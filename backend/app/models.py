9df1528fae0c739ba479b93e7b9f37360884e4bf
mcverter <mitchell.verter@gmail.com>
2024-09-13 12:52:57 -0400

    adding appropriate crud field

--- backend/app/models.py
+++ backend/app/models.py
@@ -113,31 +113,22 @@ class NewPassword(SQLModel):
     token: str
     new_password: str = Field(min_length=8, max_length=40)
 
-
-
 # Shared properties
 class InquiryBase(SQLModel):
-    title: str = Field(min_length=1, max_length=255)
-    description: str | None = Field(default=None, max_length=255)
-
+    text: str = Field(min_length=10, max_length=255)
 
 # Properties to receive on inquiry creation
 class InquiryCreate(InquiryBase):
-    title: str = Field(min_length=1, max_length=255)
+    text: str = Field(min_length=10, max_length=255)
 
 
 # Database model, database table inferred from class name
 class Inquiry(InquiryBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    title: str = Field(max_length=255)
-    owner_id: uuid.UUID = Field(
-        foreign_key="user.id", nullable=False, ondelete="CASCADE"
-    )
-    owner: User | None = Relationship(back_populates="inquirys")
+    text: str = Field(min_length=10, max_length=255)
 
 
 # Properties to return via API, id is always required
 class InquiryPublic(InquiryBase):
     id: uuid.UUID
-    owner_id: uuid.UUID
 
