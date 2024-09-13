36a34c6ec18dfee059591b3578428d6c1b747e09
mcverter <mitchell.verter@gmail.com>
2024-09-13 12:38:28 -0400

    only create

---
+++ backend/app/api/routes/inquiries.py
@@ -0,0 +1,24 @@
+import uuid
+from typing import Any
+
+from fastapi import APIRouter, HTTPException
+from sqlmodel import func, select
+
+from app.api.deps import CurrentUser, SessionDep
+from app.models import Inquiry, InquiryCreate, InquiryPublic, InquirysPublic, InquiryUpdate, Message
+
+router = APIRouter()
+
+
+@router.post("/", response_model=InquiryPublic)
+def create_inquiry(
+    *, session: SessionDep, current_user: CurrentUser, inquiry_in: InquiryCreate
+) -> Any:
+    """
+    Create new inquiry.
+    """
+    inquiry = Inquiry.model_validate(inquiry_in, update={"owner_id": current_user.id})
+    session.add(inquiry)
+    session.commit()
+    session.refresh(inquiry)
+    return inquiry
