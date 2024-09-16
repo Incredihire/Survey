05719bbe651baeb8082b76f2d2656cbb94a8701c
Neil Bae <nbae0127@gmail.com>
2024-09-16 12:19:47 -0400

    Add tests for reading all inquiries

--- backend/app/tests/crud/test_inquiry.py
+++ backend/app/tests/crud/test_inquiry.py
@@ -13,3 +13,13 @@ def test_create_inquiry(db: Session) -> None:
     inquiry_in = InquiryCreate(text=text)
     inquiry = crud.create_inquiry(session=db, inquiry_in=inquiry_in)
     assert inquiry.text == text
+
+
+def test_get_inquiry(db: Session) -> None:
+    length = random.randint(10, 255)
+    text = "".join(random.choices(string.ascii_lowercase, k=length)).capitalize()
+    inquiry_in = InquiryCreate(text=text)
+    created_inquiry = crud.create_inquiry(session=db, inquiry_in=inquiry_in)
+    retrieved_inquiry = crud.get_inquiry_by_text(session=db, text=text)
+    assert retrieved_inquiry
+    assert retrieved_inquiry.text == created_inquiry.text
