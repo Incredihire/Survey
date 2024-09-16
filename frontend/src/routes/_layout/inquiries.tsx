1d7e12221a93c7fa9e09b64f1b7f61f1a373a37d
Neil Bae <nbae0127@gmail.com>
2024-09-16 15:30:17 -0400

    Add inquiries API response model and display inquiries table

--- frontend/src/routes/_layout/inquiries.tsx
+++ frontend/src/routes/_layout/inquiries.tsx
@@ -1,27 +1,37 @@
 import {
   Container,
   Heading,
-} from "@chakra-ui/react"
-import { createFileRoute} from "@tanstack/react-router"
-import { z } from "zod"
+  SkeletonText,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { createFileRoute } from "@tanstack/react-router";
+import { z } from "zod";
 
-import Navbar from "../../components/Common/Navbar"
-import AddInquiry from "../../components/Inquiries/AddInquiry"
+import * as InquiriesService from "../../client/services/inquiriesService";
+import Navbar from "../../components/Common/Navbar";
+import AddInquiry from "../../components/Inquiries/AddInquiry";
 
 // Already typed by zod library https://zod.dev/
 // eslint-disable-next-line
 const inquiriesSearchSchema = z.object({
-// eslint-disable-next-line
+  // eslint-disable-next-line
   page: z.number().catch(1),
-})
+});
 
 // createFileRoute is already typed by tanstack-router: https://tanstack.com/router/latest/docs/framework/react/guide/type-safety
 // eslint-disable-next-line
 export const Route = createFileRoute("/_layout/inquiries")({
   component: Inquiries,
-    // eslint-disable-next-line
+  // eslint-disable-next-line
   validateSearch: (search) => inquiriesSearchSchema.parse(search),
-})
+});
 
 function Inquiries() {
   return (
@@ -31,6 +41,52 @@ function Inquiries() {
       </Heading>
 
       <Navbar type={"Inquiry"} addModalAs={AddInquiry} />
+      <InquiriesTable />
     </Container>
-  )
+  );
+}
+
+function InquiriesTable() {
+  const { data: inquiries, isPending } = useQuery({
+    queryKey: ["inquiries"],
+    queryFn: () => InquiriesService.readInquiries(),
+  });
+
+  console.log(inquiries);
+  return (
+    <>
+      <TableContainer>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>ID</Th>
+              <Th>Text</Th>
+              <Th>Created At</Th>
+            </Tr>
+          </Thead>
+          {isPending ? (
+            <Tbody>
+              <Tr>
+                {new Array(3).fill(null).map((_, index) => (
+                  <Td key={index}>
+                    <SkeletonText noOfLines={1} />
+                  </Td>
+                ))}
+              </Tr>
+            </Tbody>
+          ) : (
+            <Tbody>
+              {inquiries?.data.map((inquiry) => (
+                <Tr key={inquiry.id}>
+                  <Td>{inquiry.id}</Td>
+                  <Td>{inquiry.text}</Td>
+                  <Td>{new Date(inquiry.created_at).toISOString()}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          )}
+        </Table>
+      </TableContainer>
+    </>
+  );
 }
