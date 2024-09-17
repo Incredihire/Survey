0313ecfb181cb1dee258ae33f967983e3f878da3
Neil Bae <nbae0127@gmail.com>
2024-09-17 14:38:36 -0400

    Shorten sort inquiry function

--- frontend/src/routes/_layout/inquiries.tsx
+++ frontend/src/routes/_layout/inquiries.tsx
@@ -64,10 +64,9 @@ function InquiriesTable() {
     ...getInquiriesQueryOptions(),
   });
 
+  // Sort inquries from Newest to oldest
   const sortedInquiries = inquiries?.data.sort((a, b) => {
-    const dateA = new Date(a.created_at).getTime();
-    const dateB = new Date(b.created_at).getTime();
-    return dateB - dateA; // Newest to oldest
+    return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
   });
 
   return (
