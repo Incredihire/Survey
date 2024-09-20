ceed2d4ec44d77824b85abdd34ee52f1550f770e
Neil Bae <nbae0127@gmail.com>
2024-09-20 17:27:14 -0400

    Add frontend unit test cases

---
+++ frontend/__tests__/inquiry/inquiries_table.test.tsx
@@ -0,0 +1,15 @@
+describe("Inquiries Table", () => {
+  it("should display empty table when there's no inquiries.", () => {})
+
+  it("should display skeleton text when data is loading.", () => {})
+
+  it("should display correct number of inquiries in table.", () => {})
+
+  it("should display correct inquiry text.", () => {})
+
+  it("should display correct inquiry created date and time.", () => {})
+
+  it("should display inquiries from newest to oldest.", () => {})
+
+  it("should log the inquiry details when clicked.", () => {})
+})
