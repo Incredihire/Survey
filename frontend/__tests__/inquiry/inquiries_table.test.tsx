b50fab99b411133bae8d64571d4a0c6220b38e57
Neil Bae <nbae0127@gmail.com>
2024-09-24 17:03:22 -0400

    Unable to render inquiresTable when useQuery is in isPending mode

--- frontend/__tests__/inquiry/inquiries_table.test.tsx
+++ frontend/__tests__/inquiry/inquiries_table.test.tsx
@@ -65,7 +65,6 @@ describe("Inquiries Table", () => {
   //     isPending: true,
   //   });
   //   render(<InquiriesTable />);
-  //   renderComponent();
   //   screen.debug();
   //   expect(screen.getByTestId("inquiry-placeholder")).toBeInTheDocument();
   // });
