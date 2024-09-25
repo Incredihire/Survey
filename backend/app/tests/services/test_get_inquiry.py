94b794f6b84c805fbbfd8e22f030b09851e57205
Neil Bae <nbae0127@gmail.com>
2024-09-25 12:57:23 -0400

    Remove old inquiry service test file

---
+++ backend/app/tests/services/test_get_inquiry.py
@@ -0,0 +1,11 @@
+# def test_get_inquiry(db: Session) -> None:
+#     length = random.randint(10, 255)
+#     text = "".join(random.choices(string.ascii_lowercase, k=length)).capitalize()
+#     inquiry_in = InquiryCreate(text=text)
+#     created_inquiry = inquiries_service.create_inquiry(
+#         session=db, inquiry_in=inquiry_in
+#     )
+#     retrieved_inquiry = db.get(Inquiry, created_inquiry.id)
+#     assert retrieved_inquiry
+#     assert retrieved_inquiry.text == created_inquiry.text
+#     assert jsonable_encoder(created_inquiry) == jsonable_encoder(retrieved_inquiry)
