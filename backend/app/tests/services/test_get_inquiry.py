a3e407de23a2437aa85e6525506d5cf70ff9ce83
Neil Bae <nbae0127@gmail.com>
2024-09-25 15:31:31 -0400

    Add inquiry service test

--- backend/app/tests/services/test_get_inquiry.py
+++ backend/app/tests/services/test_get_inquiry.py
@@ -1,11 +1,26 @@
-# def test_get_inquiry(db: Session) -> None:
-#     length = random.randint(10, 255)
-#     text = "".join(random.choices(string.ascii_lowercase, k=length)).capitalize()
-#     inquiry_in = InquiryCreate(text=text)
-#     created_inquiry = inquiries_service.create_inquiry(
-#         session=db, inquiry_in=inquiry_in
-#     )
-#     retrieved_inquiry = db.get(Inquiry, created_inquiry.id)
-#     assert retrieved_inquiry
-#     assert retrieved_inquiry.text == created_inquiry.text
-#     assert jsonable_encoder(created_inquiry) == jsonable_encoder(retrieved_inquiry)
+import pytest
+from sqlmodel import Session
+
+from app.models import Inquiry
+from app.services.inquiries import get_inquiry_by_id
+
+# Naming Convention:
+# <FUT>_when<Scenario>_should<Outcome>
+# ex. addTwoNumbers_whenOneNumberIsZero_shouldReturnTheSecondNumber
+
+
+@pytest.fixture(scope="module")
+def setup_data(db: Session) -> Inquiry:
+    single_inquiry = Inquiry(text="How's your work-life balance?")
+    db.add(single_inquiry)
+    db.commit()
+    return single_inquiry
+
+
+def getSingleInquiryService_whenCalledWithValidId_shouldReturnMatchingInquiry(
+    db: Session, sample_inquiry: Inquiry
+) -> None:
+    result = get_inquiry_by_id(session=db, inquiry_id=sample_inquiry.id)
+    assert result is not None
+    assert result.id == sample_inquiry.id
+    assert result.text == sample_inquiry.text
