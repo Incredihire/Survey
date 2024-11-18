523ebd51aaf013987902087c404c6f1a3a399ff1
rashmiiyengar <rashmiiyengar255@gmail.com>
2024-11-18 13:23:32 -0800

    Refactored and moved assertions to spec files and page objects only have the locator logic

--- frontend/e2e/page-objects/InquiryPage.ts
+++ frontend/e2e/page-objects/InquiryPage.ts
@@ -1,75 +1,40 @@
-import { expect } from "@playwright/test"
 import HelperBase from "./helperBase"
 
 export default class InquiryPage extends HelperBase {
   async navigateToInquiriesPage() {
     await this.page.waitForSelector("p.chakra-text.css-0", { timeout: 5000 })
-    await expect(this.page.locator("p.chakra-text.css-0")).toHaveText(
-      "Welcome back, nice to see you again!",
-    )
     await this.page.getByText("Inquiries").click()
   }
 
   async openInquiryForm() {
-    const inquiryTitle = this.page.getByRole("heading", {
-      name: "Inquiries Management",
-    })
-    await expect(inquiryTitle).toHaveText("Inquiries Management")
     await this.page.getByRole("button", { name: "Inquiry" }).click()
     await this.page.waitForSelector(".chakra-modal__footer", {
       state: "visible",
     })
-    await expect(
-      this.page.getByLabel("Add Inquiry").getByText("Add Inquiry"),
-    ).toHaveText("Add Inquiry")
+  }
+
+  async addInquiry(inputText: string) {
+    await this.fillTextByTestId("add-inquiry-text", inputText)
+    await this.assertButtonTextByTestIdAndClick("submit-add-inquiry", "Save")
   }
 
   async addInquiry_whenValidInquiryIsAdded_shouldShowInquiryInInquiriesList(
     inputText: string,
   ) {
-    await this.navigateToInquiriesPage()
-    await this.openInquiryForm()
-    await this.fillTextByTestId("add-inquiry-text", inputText)
-    await this.assertButtonTextByTestIdAndClick("submit-add-inquiry", "Save")
-    await expect(this.page.locator(`text=${inputText}`)).toBeVisible()
+    await this.addInquiry(inputText)
   }
 
   async inquiry_whenInvalidInquiryText_shouldShowValidationError(
     inputText: string,
   ) {
-    await this.navigateToInquiriesPage()
-    await this.openInquiryForm()
-    await this.fillTextByTestId("add-inquiry-text", inputText)
-    if (inputText.length === 0) {
-      await this.assertButtonTextByTestIdAndClick("submit-add-inquiry", "Save")
-      await expect(this.page.getByText("Inquiry text is required.")).toHaveText(
-        "Inquiry text is required.",
-      )
-    } else if (inputText.length < 10) {
-      await this.assertButtonTextByTestIdAndClick("submit-add-inquiry", "Save")
-      await expect(
-        this.page.getByText("Inquiry must be at least 10 characters."),
-      ).toHaveText("Inquiry must be at least 10 characters.")
-    } else if (inputText.length > 256) {
-      await this.assertButtonTextByTestIdAndClick("submit-add-inquiry", "Save")
-      await expect(
-        this.page.getByText("Inquiry can not be greater than 256 characters."),
-      ).toHaveText("Inquiry can not be greater than 256 characters.")
-    } else {
-      await this.assertButtonTextByTestIdAndClick("submit-add-inquiry", "Save")
-    }
+    await this.addInquiry(inputText)
   }
 
   async dismissPopupUsingCancel() {
-    await this.navigateToInquiriesPage()
-    await this.openInquiryForm()
     await this.page.getByRole("button", { name: "Cancel" }).click()
-    await expect(this.page.getByRole("button", { name: "Cancel" })).toBeHidden()
   }
+
   async dismissPopupUsingX() {
-    await this.navigateToInquiriesPage()
-    await this.openInquiryForm()
     await this.page.locator(".chakra-modal__close-btn").click()
-    await expect(this.page.locator(".chakra-modal__close-btn")).toBeHidden()
   }
 }
--- frontend/e2e/tests/inquiry.spec.ts
+++ frontend/e2e/tests/inquiry.spec.ts
@@ -1,4 +1,4 @@
-import { test } from "@playwright/test"
+import { expect, test } from "@playwright/test"
 import PageManager from "../page-objects/pageManager"
 
 test.describe("Inquiry Management Suite", () => {
@@ -15,68 +15,95 @@ test.describe("Inquiry Management Suite", () => {
   }) => {
     const pm = new PageManager(page)
     await pm.oninquiryPage()
+    await pm.oninquiryPage().navigateToInquiriesPage()
+    await pm.oninquiryPage().openInquiryForm()
+
+    const inputText = "How would you rate the work environment in your team?"
     await pm
       .oninquiryPage()
       .addInquiry_whenValidInquiryIsAdded_shouldShowInquiryInInquiriesList(
-        "How would you rate the work environment in your team?",
+        inputText,
       )
+
+    await expect(page.locator(`text=${inputText}`)).toBeVisible()
   })
-  test("TC_002 Verify error messages appear for empty input fields ", async ({
+
+  test("TC_002 Verify error messages appear for empty input fields", async ({
     page,
   }) => {
     const pm = new PageManager(page)
     await pm.oninquiryPage()
+    await pm.oninquiryPage().navigateToInquiriesPage()
+    await pm.oninquiryPage().openInquiryForm()
+
+    const inputText = ""
     await pm
       .oninquiryPage()
-      .inquiry_whenInvalidInquiryText_shouldShowValidationError("")
+      .inquiry_whenInvalidInquiryText_shouldShowValidationError(inputText)
+
+    await expect(page.getByText("Inquiry text is required.")).toHaveText(
+      "Inquiry text is required.",
+    )
   })
 
-  test("TC_003 Verify error message appears for input field with greater than 256 characters.", async ({
+  test("TC_003 Verify error message appears for input field with greater than 256 characters", async ({
     page,
   }) => {
-    const inputPhrase =
+    const inputText =
       "In a world where technology is advancing at an exponential rate, it's important to remember the value of human connection, empathy, and kindness. As we move forward, let's not forget the importance of collaboration creativity and the pursuit of happiness in world."
     const pm = new PageManager(page)
     await pm.oninquiryPage()
+    await pm.oninquiryPage().navigateToInquiriesPage()
+    await pm.oninquiryPage().openInquiryForm()
+
     await pm
       .oninquiryPage()
-      .inquiry_whenInvalidInquiryText_shouldShowValidationError(inputPhrase)
+      .inquiry_whenInvalidInquiryText_shouldShowValidationError(inputText)
+
+    await expect(
+      page.getByText("Inquiry can not be greater than 256 characters."),
+    ).toHaveText("Inquiry can not be greater than 256 characters.")
   })
 
-  test("TC_004 Verify error message appears for input field with greater than 256 characters.", async ({
+  test("TC_004 Verify error message appears for input field with less than 10 characters", async ({
     page,
   }) => {
-    const inputPhrase = "whats up?"
+    const inputText = "whats up?"
     const pm = new PageManager(page)
     await pm.oninquiryPage()
+    await pm.oninquiryPage().navigateToInquiriesPage()
+    await pm.oninquiryPage().openInquiryForm()
+
     await pm
       .oninquiryPage()
-      .inquiry_whenInvalidInquiryText_shouldShowValidationError(inputPhrase)
+      .inquiry_whenInvalidInquiryText_shouldShowValidationError(inputText)
+
+    await expect(
+      page.getByText("Inquiry must be at least 10 characters."),
+    ).toHaveText("Inquiry must be at least 10 characters.")
   })
 
   test("TC_005 Verify the behavior of Cancel button", async ({ page }) => {
     const pm = new PageManager(page)
     await pm.oninquiryPage()
+    await pm.oninquiryPage().navigateToInquiriesPage()
+    await pm.oninquiryPage().openInquiryForm()
+
     await pm.oninquiryPage().dismissPopupUsingCancel()
+
+    await expect(page.getByRole("button", { name: "Cancel" })).toBeHidden()
   })
 
-  test("TC_005 Verify the behavior of X button to cancel popup", async ({
+  test("TC_006 Verify the behavior of X button to cancel popup", async ({
     page,
   }) => {
     const pm = new PageManager(page)
     await pm.oninquiryPage()
+    await pm.oninquiryPage().navigateToInquiriesPage()
+    await pm.oninquiryPage().openInquiryForm()
+
     await pm.oninquiryPage().dismissPopupUsingX()
-  })
 
-  test("TC_006 Verify the presence of AddInquiry button on Inquiry Management page", async ({
-    page,
-  }) => {
-    const pm = new PageManager(page)
-    await pm.oninquiryPage()
-    await pm
-      .oninquiryPage()
-      .addInquiry_whenValidInquiryIsAdded_shouldShowInquiryInInquiriesList(
-        "How was your week",
-      )
+    await expect(page.locator(".chakra-modal__close-btn")).toBeHidden()
   })
 })
