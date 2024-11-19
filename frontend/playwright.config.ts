a7303a927a6f42ee98546abb6a4f535962dde274
rashmiiyengar <rashmiiyengar255@gmail.com>
2024-11-19 15:01:01 -0800

    dont run playwright tests in parallel mode temporarily disable auth.setup.ts

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -13,7 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e/tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -33,7 +33,7 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    { name: 'setup', testMatch: /.*\.setup\.ts/ },
+    //{ name: 'setup', testMatch: /.*\.setup\.ts/ },
 
     {
       name: 'chromium',
@@ -41,7 +41,7 @@ export default defineConfig({
         ...devices['Desktop Chrome'],
         //storageState: 'playwright/.auth/user.json',
       },
-      dependencies: ['setup'],
+    //dependencies: ['setup'],
     },
 
     // {
