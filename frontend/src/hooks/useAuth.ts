8fd76a7f846aa3356fae527ccf0d62f40fe70780
Jereme Willig <jereme@jeremewillig.com>
2024-11-24 08:33:24 -1000

    Fix new codacy lint warnings

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
 import { useState } from "react"
 
-import type { UserPublic } from "../client"
 import { UsersService } from "../client/services"
 
 const useAuth = () => {
@@ -11,7 +10,7 @@ const useAuth = () => {
     isLoading,
     refetch,
     failureReason,
-  } = useQuery<UserPublic, Error>({
+  } = useQuery({
     queryKey: ["currentUser"],
     // eslint-disable-next-line @typescript-eslint/unbound-method
     queryFn: UsersService.readUserMe,
@@ -22,9 +21,13 @@ const useAuth = () => {
       window.location.href = escape("/api/v1/auth/login")
     }
     fetch("/api/v1/auth/refresh", { method: "POST" })
-      .then((response) =>
-        response.ok ? refetch().catch(gotoLogin) : gotoLogin(),
-      )
+      .then((response) => {
+        if (response.ok) {
+          refetch().catch(gotoLogin)
+        } else {
+          gotoLogin()
+        }
+      })
       .catch(gotoLogin)
   }
   return {
