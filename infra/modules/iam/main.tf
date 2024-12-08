aa6bbef28694011467fe59a54fdfe0de57cb1dbc
ramki11 <ratnakara.ramki@gmail.com>
2024-12-07 19:38:24 -0500

    iam role

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -15,31 +15,38 @@ resource "aws_iam_role" "ecs_task_execution" {
   ]
 }
 EOF
-}
 
-resource "aws_iam_policy" "ecs_secrets_manager_access" {
-  name        = "ecs_secrets_manager_access"
-  description = "Allows ECS tasks to access the secret in AWS Secrets Manager"
-  policy      = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "secretsmanager:GetSecretValue"
-        ],
-        Resource = "arn:aws:secretsmanager:us-west-2:913524926070:secret:staging/survey/rds-6cId0d"
-      }
-    ]
-  })
+  inline_policy {
+    name = "ecs_secrets_and_logs_access"
+
+    policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Effect = "Allow",
+          Action = [
+            "secretsmanager:GetSecretValue"
+          ],
+          Resource = "arn:aws:secretsmanager:us-west-2:913524926070:secret:staging/survey/rds-6cId0d"
+        },
+        {
+          Effect = "Allow",
+          Action = [
+            "logs:CreateLogGroup",
+            "logs:PutRetentionPolicy",
+            "logs:TagResource"
+          ],
+          Resource = [
+            "arn:aws:logs:us-west-2:913524926070:log-group:survey-backend-logs:*",
+            "arn:aws:logs:us-west-2:913524926070:log-group:survey-frontend-logs:*"
+          ]
+        }
+      ]
+    })
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_secrets_manager_policy_attachment" {
-  role       = aws_iam_role.ecs_task_execution.name
-  policy_arn = aws_iam_policy.ecs_secrets_manager_access.arn
 }
