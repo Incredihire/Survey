7664230c3f7705b70d74e2387a96475a3e7b94e9
ramki11 <ratnakara.ramki@gmail.com>
2024-12-07 19:26:15 -0500

    SM role

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -1,25 +1,45 @@
-#IAM role for ecs task
 resource "aws_iam_role" "ecs_task_execution" {
   name = "iam_role_for_ecs_task_exec"
 
   assume_role_policy = <<EOF
 {
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": "sts:AssumeRole",
-     "Principal": {
-       "Service": "ecs-tasks.amazonaws.com"
-     },
-     "Effect": "Allow",
-     "Sid": ""
-   }
- ]
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
 }
 EOF
+}
+
+resource "aws_iam_policy" "ecs_secrets_manager_access" {
+  name        = "ecs_secrets_manager_access"
+  description = "Allows ECS tasks to access the secret in AWS Secrets Manager"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ],
+        Resource = "arn:aws:secretsmanager:us-west-2:913524926070:secret:staging/survey/rds-6cId0d"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_secrets_manager_policy_attachment" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = aws_iam_policy.ecs_secrets_manager_access.arn
 }
