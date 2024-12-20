d7d7e94aad93ba8a720256128752f7f07d335299
ramki11 <ratnakara.ramki@gmail.com>
2024-12-20 10:53:52 -0500

    image path

--- infra/main.tf
+++ infra/main.tf
@@ -50,7 +50,9 @@ module "ecs-backend" {
   load_balancer_security_group_id = module.alb-backend.alb_security_group_id
   target_group_arn                = module.alb-backend.target_group_arn
   ecs_task_execution_role_arn     = module.iam.ecs_task_execution_role_arn
-  image_path                      = "{var.AWS-accountID}.dkr.ecr.{var.region}.amazonaws.com/survey_backend"
+  #image_path                      = "{var.AWS-accountID}.dkr.ecr.{var.region}.amazonaws.com/survey_backend"
+  image_path = "${var.AWS-accountID}.dkr.ecr.${var.region}.amazonaws.com/survey_backend"
+
   image_tag                       = var.image_tag
 }
 
@@ -78,7 +80,9 @@ module "ecs-frontend" {
   load_balancer_security_group_id = module.alb-frontend.alb_security_group_id
   target_group_arn                = module.alb-frontend.target_group_arn
   ecs_task_execution_role_arn     = module.iam.ecs_task_execution_role_arn
-  image_path                      = "{var.AWS-accountID}.dkr.ecr.{var.region}.amazonaws.com/survey_frontend"
+  #image_path                      = "{var.AWS-accountID}.dkr.ecr.{var.region}.amazonaws.com/survey_frontend"
+  image_path = "${var.AWS-accountID}.dkr.ecr.${var.region}.amazonaws.com/survey_frontend"
+
   image_tag                       = var.image_tag
 }
 
