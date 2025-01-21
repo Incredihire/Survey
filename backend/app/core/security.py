df3c49efecd11cc4bf9ac6910c16e098cdd1839c
Jereme Willig <jwillig@avantia-inc.com>
2025-01-21 03:19:14 -1000

    remove unused GOOGLE_CLIENT_CONFIG and SCOPES from security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -7,37 +7,6 @@ from passlib.context import CryptContext
 
 from app.core.config import settings
 
-GOOGLE_CLIENT_CONFIG = {
-    "web": {
-        "client_id": f"{settings.GOOGLE_CLIENT_ID}",
-        "project_id": "survey-incredihire",
-        "auth_uri": f"{settings.GOOGLE_AUTHORIZATION_URL}",
-        "token_uri": f"{settings.GOOGLE_TOKEN_URL}",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_secret": f"{settings.GOOGLE_CLIENT_SECRET}",
-        "redirect_uris": [
-            "https://survey.incredihire.com",
-            "https://survey-api.incredihire.com/api/v1/auth/callback",
-            "https://survey-api.incredihire.com/docs/oauth2-redirect",
-            "http://localhost/",
-            "http://localhost/api/v1/auth/callback",
-            "http://localhost/docs/oauth2-redirect",
-            "http://local.jeremewillig.com/api/v1/auth/callback",
-        ],
-        "javascript_origins": [
-            "https://localhost:8000",
-            "https://survey.incredihire.com",
-            "https://localhost",
-        ],
-    }
-}
-
-SCOPES = [
-    "openid",
-    "https://www.googleapis.com/auth/userinfo.email",
-    "https://www.googleapis.com/auth/userinfo.profile",
-]
-
 access_security = JwtAccessBearerCookie(
     secret_key=settings.JWT_SECRET_KEY,
     auto_error=False,
