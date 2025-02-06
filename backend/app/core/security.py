d1fa3b921016dc5032a526bbb7d1aca00cfa69c5
Jereme Willig <jwillig@avantia-inc.com>
2025-02-05 22:05:41 -1000

    Requests call without timeout - bugfix

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -10,7 +10,7 @@ oidc_auth = OpenIdConnect(openIdConnectUrl=settings.OPENID_CONNECT_URL)
 
 def _fetch_well_known() -> dict[str, Any]:
     url = f"{settings.OIDC_ISSUER}/.well-known/openid-configuration"
-    with requests.get(url) as response:
+    with requests.get(url, timeout=30) as response:
         if response.status_code != 200:
             raise RuntimeError("fail to fetch well-known")
         well_known: dict[str, Any] = response.json()
@@ -25,7 +25,7 @@ algorithms: list[str] = _well_known_doc["id_token_signing_alg_values_supported"]
 
 def _fetch_jwks() -> dict[str, Any]:
     url = _well_known_doc["jwks_uri"]
-    with requests.get(url) as response:
+    with requests.get(url, timeout=30) as response:
         if response.status_code != 200:
             raise RuntimeError("fail to fetch jwks")
         _jwks: dict[str, Any] = response.json()
